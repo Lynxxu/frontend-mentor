ec64396657850481fd307c67c02e38981f433725
Lynx <75997156+Lynxxu@users.noreply.github.com>
2023-12-30 11:21:29 -0500

    first try on front-end mentor QR code

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,18 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen justify-center flex-col items-center p-24 ${inter.className}`}
+      className={`flex min-h-screen justify-center flex-col items-center p-24`}
     >
-      <p className="text-2xl">
-        Frontend-mentor <Link href="/posts/QR-code-challenge">solution </Link>{" "}
-        sites
-      </p>
-      <p>Created by Xiangbo </p>
+      <div className="text-center w-full h-full">
+        <p className="text-2xl font-medium">
+          Frontend-mentor{" "}
+          <Link href="/posts/QR-Code-Challenge/main">solution</Link> sites
+        </p>
+        <p>Created by Xiangbo </p>
+      </div>
+      <div className=""></div>
     </main>
   );
 }
